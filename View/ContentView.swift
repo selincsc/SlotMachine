c4e8ad225b0b1f79d6ddb9ada4da18e8bdc1c666
Selin Çağlar <58820720+selincsc@users.noreply.github.com>
2023-01-18 21:52:55 +0300

    reset button and showing modal

--- View/ContentView.swift
+++ View/ContentView.swift
@@ -13,11 +13,14 @@ struct ContentView: View {
     
     let symbols = ["gfx-bell", "gfx-cherry", "gfx-coin", "gfx-grape", "gfx-seven", "gfx-strawberry"]
     
-    @State private var highscore: Int = 0
+    @State private var highscore: Int = UserDefaults.standard.integer(forKey: "HighScore")
     @State private var coins: Int = 100
     @State private var betAmount: Int = 10
     @State private var reels: Array =  [0, 1, 2]
     @State private var showingInfoView: Bool = false
+    @State private var isActiveBet10: Bool = true
+    @State private var isActiveBet20: Bool = false
+    @State private var showingModal: Bool = false
     
 //MARK: -  FUNCTIONS
     
@@ -47,13 +50,36 @@ struct ContentView: View {
     }
     func newHighScore() {
         highscore = coins
+        UserDefaults.standard.set(highscore, forKey: "HighScore")
     }
     func playerLoses() {
         coins -= betAmount
     }
+    func activateBet20() {
+        betAmount = 20
+        isActiveBet20 = true
+        isActiveBet10 = false
+    }
+    func activateBet10() {
+        betAmount = 10
+        isActiveBet10 = true
+        isActiveBet20 = false
+    }
+    func isGameOver() {
+        if coins <= 0 {
+            //show modal window
+            showingModal = true
+        }
+    }
+    func resetGame(){
+        UserDefaults.standard.set(0, forKey: "HighScore")
+        highscore = 0
+        coins = 100
+        activateBet10()
+    }
 
 
-//GAME IS OVER
+
     
 //MARK: - BODY
     var body: some View {
@@ -126,6 +152,8 @@ struct ContentView: View {
                         self.spinReels()
                         //CHECKWINNNINGS
                         self.checkWinning()
+                        //GAME IS OVER
+                        self.isGameOver()
                     }){
                         Image("gfx-spin")
                             .renderingMode(.original)
@@ -143,32 +171,32 @@ struct ContentView: View {
                     //MARK: bet 20
                     HStack (alignment: .center, spacing: 10){
                         Button(action: {
-                            print("Bet 20 coins")
+                            self.activateBet20()
                         }){
                             Text("20")
                                 .fontWeight(.heavy)
-                                .foregroundColor(Color.white)
+                                .foregroundColor(isActiveBet20 ? Color("ColorYellow") : Color.white)
                                 .modifier(BetNumberModifiers())
                         }
                         .modifier(BetCapsuleModifiers())
                         Image("gfx-casino-chips")
                             .resizable()
-                            .opacity(0)
+                            .opacity(isActiveBet20 ? 1 : 0)
                             .modifier(CasinoChipsModifiers())
                     }
                     //MARK: bet 10
                     HStack (alignment: .center, spacing: 10){
                         Image("gfx-casino-chips")
                             .resizable()
-                            .opacity(1)
+                            .opacity(isActiveBet10 ? 1 : 0)
                             .modifier(CasinoChipsModifiers())
                         
                         Button(action: {
-                            print("Bet 10 coins")
+                            self.activateBet10()
                         }){
                             Text("10")
                                 .fontWeight(.heavy)
-                                .foregroundColor(Color.yellow)
+                                .foregroundColor(isActiveBet10 ? Color("ColorYellow") : Color.white)
                                 .modifier(BetNumberModifiers())
                         }
                         .modifier(BetCapsuleModifiers())
@@ -181,7 +209,7 @@ struct ContentView: View {
             .overlay(
                 //MARK: - RESET
                 Button(action: {
-                    print("Reset the game")
+                    self.resetGame()
                 }){
                     Image(systemName: "arrow.2.circlepath.circle")
                 }
@@ -198,8 +226,62 @@ struct ContentView: View {
             )
             .padding()
             .frame(maxWidth: 720)
+            .blur(radius: $showingModal.wrappedValue ? 5 : 0, opaque: false)
             
                 //MARK: - POPUP
+            if $showingModal.wrappedValue {
+                ZStack{
+                    Color("ColorTransparentBlack").edgesIgnoringSafeArea(.all)
+                    //modal
+                    VStack(spacing: 0){
+                        //title
+                        Text("GAME OVER")
+                            .font(.system(.title, design: .rounded))
+                            .fontWeight(.heavy)
+                            .padding()
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .background(Color("ColorPink"))
+                            .foregroundColor(Color.white)
+                        Spacer()
+                        //message
+                        VStack (alignment: .center, spacing: 16){
+                            Image("gfx-seven-reel")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxHeight: 72)
+                            
+                            Text("Bad luck ! You lost all of the coins. \nLet's play again !")
+                                .font(.system(.body, design: .rounded))
+                                .lineLimit(2)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(Color.gray)
+                                .layoutPriority(1)
+                            Button(action: {
+                                self.showingModal = false
+                                self.coins = 100
+                            }){
+                                Text("New Game".uppercased())
+                                    .font(.system(.body, design: .rounded))
+                                    .fontWeight(.semibold)
+                                    .accentColor(Color("ColorPink"))
+                                    .padding(.horizontal, 12)
+                                    .padding(.vertical, 8)
+                                    .frame(minWidth: 128)
+                                    .background(
+                                        Capsule()
+                                            .strokeBorder(lineWidth: 1.75)
+                                            .foregroundColor(Color("ColorPink"))
+                                    )
+                            }
+                        }
+                        Spacer()
+                    }
+                    .frame(minWidth: 280, idealWidth: 280, maxWidth: 320, minHeight: 260, idealHeight: 280, maxHeight: 320, alignment: .center)
+                    .background(Color.white)
+                    .cornerRadius(20)
+                    .shadow(color: Color("ColorTransparentBlack"), radius: 6, x: 0, y: 8)
+                }
+            }
         }//:zstack
         .sheet(isPresented: $showingInfoView){
             InfoView()
